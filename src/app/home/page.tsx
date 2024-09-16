f435a81b37da6432b00a7d11e54affbda206e657
maxng17 <maxngjl@gmail.com>
2024-09-16 17:47:22 +0800

    Simple fetching of user data

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,16 @@
+import { auth } from "@clerk/nextjs/server";
+
 export default function StartPage() {
+
+    const {userId} = auth();
+
     return (
         <div>
             <h1>
                 this is the home page
+            </h1>
+            <h1>
+                Hello, {userId}
             </h1>
         </div>
     )
