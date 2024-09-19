00099c43540ec4e539d62ed8c115c9522b9f5a4e
maxng17 <maxngjl@gmail.com>
2024-09-19 15:12:39 +0800

    Update background image of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import Link from "next/link";
+
 
 export default async function HomePage() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="font-bold text-3xl">
-        Govtech Annual Soccer
-      </h1>
-      <Link href='/sign-up'>
-        <span className="text-blue-500 underline text-lg hover:text-blue-700">
-          Create an account now!
-        </span>
-      </Link>
+    // Image credited to to google gemini (AI created)
+    <main 
+      className="flex min-h-screen flex-col items-center justify-center bg-center"
+      style={{
+        backgroundImage: "url(./assets/govtech_football.jpg)",
+        backgroundSize: "100% 100%",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
     </main>
   );
 }
