a70c7bc710f2bf1d6e3a5bef693826d5b26286e5
maxng17 <maxngjl@gmail.com>
2024-09-19 14:45:49 +0800

    Add some description of page

--- src/app/_components/logsTable.tsx
+++ src/app/_components/logsTable.tsx
@@ -9,6 +9,17 @@ export default function LogsTable( {logDatas}  : LeaderTableProps) {
     return (
         <div className="w-full">
             <h2 className="text-xl font-bold mb-2 text-center">Logs of all data changes</h2>
+            <div className="mb-6 text-center">
+                <p className="mt-2 text-gray-600">
+                    This is the log page. Here, you can view records of actions performed.
+                </p>
+                <p className="mt-1 text-gray-600">
+                    The operations logged are either <strong>delete</strong>, <strong>add</strong>, or <strong>edit</strong>.
+                </p>
+                <p className="mt-1 text-gray-600">
+                    The data types logged include <strong>team data</strong> and <strong>match results data</strong>.
+                </p>
+            </div>
             <table className="table-fixed w-full border-collapse border border-gray-200">
                 <thead>
                     <tr>
